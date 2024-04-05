e0d293ec106ce1f32904b1034045b6de702250be
root <root@DESKTOP-SVMU245>
2024-04-05 16:07:04 +0800

    delete

--- re510_slam/rs_mcl/src/mcl.h
+++ re510_slam/rs_mcl/src/mcl.h
@@ -13,7 +13,8 @@
 
 class mcl
 {
-  struct particle{
+  struct particle
+  {
     Eigen::Matrix4f pose;
     float score;
     Eigen::Matrix4Xf scan; // Only for maximum probability particle.
@@ -22,8 +23,8 @@ class mcl
 private:
   int m_sync_count;
 
-  std::random_device rd;  //Will be used to obtain a seed for the random number engine
-  std::mt19937 gen; //Standard mersenne_twister_engine seeded with rd()
+  std::random_device rd; // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen;      // Standard mersenne_twister_engine seeded with rd()
 
   float imageResolution;
   float mapCenterX;
@@ -32,7 +33,7 @@ private:
   int numOfParticle;
   std::vector<particle> particles;
   particle maxProbParticle;
-  cv::Mat gridMap; // Gridmap for showing
+  cv::Mat gridMap;   // Gridmap for showing
   cv::Mat gridMapCV; // Gridmap for use (gaussian-blurred)
   Eigen::Matrix4f tf_laser2robot;
   Eigen::Matrix4f odomBefore;
@@ -49,14 +50,10 @@ private:
   void resampling();
   void showInMap();
 
-
 public:
   mcl();
   ~mcl();
   void updateData(Eigen::Matrix4f pose, Eigen::Matrix4Xf laser);
 };
 
-
-
 #endif
-
